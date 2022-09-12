c2d6f22b2e19e33b957e96e22d802d6f82b9c9a3
Raphael Castro <rafaphcastro@gmail.com>
2022-09-12 17:25:44 -0300

    chore: improve tests

--- domain/entities/user_test.go
+++ domain/entities/user_test.go
@@ -16,13 +16,17 @@ func TestUser(t *testing.T) {
 var _ = Describe("domain/entities/user", func() {
 	Context("NewUser", func() {
 		It("should return an error when id is invalid", func() {
+			// given/when
 			user, err := entities.NewUser(faker.Word())
+			// then
 			Expect(user).To(BeNil())
 			Expect(err).ToNot(BeNil())
 		})
 
 		It("should return an user when id is valid", func() {
+			// given/when
 			user, err := entities.NewUser(faker.UUIDHyphenated())
+			// then
 			Expect(user).ToNot(BeNil())
 			Expect(err).To(BeNil())
 		})
