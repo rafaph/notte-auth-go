22d8480a03893d1d3fbf58bd5d1831a58e9112ab
Raphael Castro <rafaphcastro@gmail.com>
2022-09-16 20:25:39 -0300

    test: notte user client

--- external/notte/http/notte_user_client_test.go
+++ external/notte/http/notte_user_client_test.go
@@ -1,14 +1,12 @@
 package http_test
 
 import (
-	"encoding/json"
 	"github.com/go-faker/faker/v4"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/rafaph/notte-auth/config"
 	. "github.com/rafaph/notte-auth/external/notte/http"
-	. "github.com/rafaph/notte-auth/infrastructure/repositories/http"
-	"github.com/rafaph/notte-auth/test/helpers"
+	. "github.com/rafaph/notte-auth/test/factories"
+	. "github.com/rafaph/notte-auth/test/helpers/http"
 	"net/http"
 	"testing"
 )
@@ -20,27 +18,52 @@ func TestNotteHttpSuite(t *testing.T) {
 
 var _ = Describe("external/notte/http/notte_user_client", func() {
 	It("should return an user response successfully", func() {
-		// given
-		fakeServer := helpers.NewFakeServer()
 		userId := faker.UUIDHyphenated()
-		body := map[string]string{"id": userId}
-		responseBody, _ := json.Marshal(body)
-		context := helpers.Context{
-			StatusCode:   http.StatusOK,
-			ResponseBody: string(responseBody),
+		response := MockResponse{
+			StatusCode: http.StatusOK,
+			Body:       map[string]string{"id": userId},
 		}
-		fakeServer.When("POST", "/users/verify").Returns(context)
-
-		fakeServer.Run(func(baseUrl string) {
-			conf := &config.UserConfig{BaseUrl: baseUrl}
-			client := NewNotteUserClient(conf)
+		NewTestNotteUserClient(response).Run(func(client *NotteUserClient) {
+			// given
+			request := MakeGetUserRequest()
 			// when
-			request := GetUserRequest{Email: faker.Email(), Password: faker.Password()}
 			response, err := client.GetUser(request)
 			// then
 			Expect(response).ToNot(BeNil())
 			Expect(response.Id).To(Equal(userId))
 			Expect(err).To(BeNil())
+		})
+	})
+
+	It("should return an error when response status is 400", func() {
+		response := MockResponse{
+			StatusCode: http.StatusBadRequest,
+			Body:       map[string]string{},
+		}
+		NewTestNotteUserClient(response).Run(func(client *NotteUserClient) {
+			// given
+			request := MakeGetUserRequest()
+			// when
+			response, err := client.GetUser(request)
+			// then
+			Expect(response).To(BeNil())
+			Expect(err).ToNot(BeNil())
+		})
+	})
+
+	It("should return an error when response is different from expected", func() {
+		response := MockResponse{
+			StatusCode: http.StatusOK,
+			Body:       map[string]string{},
+		}
+		NewTestNotteUserClient(response).Run(func(client *NotteUserClient) {
+			// given
+			request := MakeGetUserRequest()
+			// when
+			response, err := client.GetUser(request)
+			// then
+			Expect(response).To(BeNil())
+			Expect(err).ToNot(BeNil())
 		})
 	})
 })
---
+++ test/helpers/http/mock_server.go
@@ -0,0 +1,78 @@
+package http
+
+import (
+	"github.com/rafaph/notte-auth/lib/validator"
+	"net/http"
+	"net/http/httptest"
+)
+
+type MockResponse struct {
+	StatusCode int
+	Body       interface{}
+}
+
+type mockRoute struct {
+	method string `validate:"required"`
+	path   string `validate:"required"`
+}
+
+type mockHandler struct {
+	route    mockRoute
+	response MockResponse
+}
+
+type mockServer struct {
+	handlers     []mockHandler
+	currentRoute mockRoute
+}
+
+func (f *mockServer) When(method string, path string) *mockServer {
+	f.currentRoute = mockRoute{method, path}
+
+	return f
+}
+
+func (f *mockServer) Return(response MockResponse) {
+	if err := validator.Validate(f.currentRoute); err != nil {
+		panic("invalid route, please call `When` first.")
+	}
+
+	f.handlers = append(f.handlers, mockHandler{f.currentRoute, response})
+	f.currentRoute = mockRoute{}
+}
+
+func (f *mockServer) getHandler() http.Handler {
+	return http.HandlerFunc(func(responseWriter http.ResponseWriter, request *http.Request) {
+		responseWriter.Header().Add("Content-Type", "application/json; charset=utf-8")
+
+		for _, handler := range f.handlers {
+			if request.URL.Path == handler.route.path {
+				if request.Method != handler.route.method {
+					responseWriter.WriteHeader(http.StatusMethodNotAllowed)
+					_, _ = responseWriter.Write([]byte(`{"message": "method not allowed"}`))
+					return
+				}
+
+				responseWriter.WriteHeader(handler.response.StatusCode)
+				_, _ = responseWriter.Write([]byte(handler.response.Body.(string)))
+				return
+			}
+		}
+		responseWriter.WriteHeader(http.StatusNotFound)
+		_, _ = responseWriter.Write([]byte(`{"message": "not found"}`))
+	})
+}
+
+func (f *mockServer) Run(callback func(baseUrl string)) {
+	server := httptest.NewServer(f.getHandler())
+	defer server.Close()
+
+	callback(server.URL)
+}
+
+func NewMockServer() *mockServer {
+	return &mockServer{
+		[]mockHandler{},
+		mockRoute{},
+	}
+}
